9f5808193b2d4a7d1427d97de085558df993a42c
MikeSC27 <mcruzen89@gmail.com>
2023-01-03 14:02:16 -0800

    testing with jest and react testing library done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className = "footer">
-      <p>Created and managed by Blake and Mike ind.</p>
+      <p>Created and managed by Blake and Mike industries</p>
     </div>
   )
 }
