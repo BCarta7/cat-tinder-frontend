6c38f3c756088448606e58c218ceb3bcbfdf1dae
BCarta7 <cannolidude@msn.com>
2023-01-04 14:05:40 -0800

    test updated

--- src/pages/CatNew.test.js
+++ src/pages/CatNew.test.js
@@ -1,14 +1,27 @@
-import { render } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
 import { BrowserRouter } from "react-router-dom"
 import CatNew from "./CatNew"
 
 describe("<CatNew />", () => {
-  it("renders without crashing", () => {
-    const div = document.createElement("div")
-    render(
-        <BrowserRouter>
-            <CatNew />
-        </BrowserRouter>,
-         div)
+    beforeEach(() => {
+        render(
+            <BrowserRouter>
+                <CatNew />
+            </BrowserRouter>
+        )
+    })
+  it("renders without crashing", () => {})
+  it("renders a form for the user", () => {
+    const formName = screen.getByText("Name")
+    expect(formName.getAttribute("For")).toEqual("name")
+
+    const formAge = screen.getByText("Age")
+    expect(formAge.getAttribute("For")).toEqual("age")
+
+    const formEnjoys = screen.getByText("Enjoys")
+    expect(formEnjoys.getAttribute("For")).toEqual("enjoys")
+
+    const formImage = screen.getByText("Image URL")
+    expect(formImage.getAttribute("For")).toEqual("image")
   })
 })
